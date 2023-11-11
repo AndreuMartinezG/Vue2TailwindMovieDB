9b64e0003cc6caeef1e8fa70584bc568709c6d78
“AndreuMartinez” <andreu.martinez@illusionstudio.es>
2023-11-11 14:55:31 +0100

    Feature: Fetch de peliculas completado

--- src/views/MoviesPage.vue
+++ src/views/MoviesPage.vue
@@ -1,53 +1,118 @@
 <template>
-  <div class="flex justify-center items-center w-auto h-screen">
-    <div class="max-w-sm max-h-sm rounded overflow-hidden shadow-xl">
-      <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
-          eaque,
-          exercitationem praesentium nihil.
-        </p>
-      </div>
-      <div class="px-6 pt-4 pb-2">
-        <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-        <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-        <span
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  <!--<CardTemplate />
+  <p class="description pt-8 text-sm">{{ firstMovieName }}</p>-->
+  <div class="max-w-5xl flex items-center h-auto lg:h-screen flex-wrap mx-auto lg:my-0">
+    <!--Main Col-->
+    <div id="profile"
+      class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-100 mx-6 lg:mx-0">
+      <div class="p-4 md:p-12 text-center lg:text-left">
+        <!-- Image for mobile view -->
+        <div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center"
+          :style="{ 'background-image': 'url(' + image + ')' }"></div>
+        <div class="form-inline flex flex-wrap content-between">
+          <input class="font-bold pt-8 lg:pt-0 lg:pl-6 justify-center mb-0 w-3/4 rounded shadow" v-model="querry"
+            placeholder="Busqueda" />
+          <button @click.prevent="newSearch()"
+            class="bg-blue-500 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow w-1/4">
+            Buscar
+          </button>
+          <span v-if="searching">
+            Buscando ....
+          </span>
+        </div>
+
+        <div class="form-inline flex flex-wrap content-between">
+          <span class="w-2/4 pt-4 text-base font-bold flex items-center justify-center lg:justify-start"><svg
+              class="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path
+                d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" />
+            </svg> Bibliografia</span>
+          <!--
+                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold mt-2 py-2 px-4 border border-gray-400 rounded shadow w-2/4" @click="showModal">Ver Letra</button>
+                    <Lyrics v-show="isModalVisible" @close="closeModal" :name="querry" ref="Lyrics" :songs="songs" />-->
+        </div>
+        <p class="description pt-8 text-sm"> {{ description | formateo }}</p>
       </div>
     </div>
+
+    <!--Img Col-->
+    <div class="w-full lg:w-2/5">
+      <!-- Big profile image for side bar (desktop) -->
+      <img :src="image" class="image-band shadow-2xl hidden lg:block">
+    </div>
+
   </div>
 </template>
 
 <script>
 
+//import CardTemplate from '@/components/CardTemplate.vue';
+import { API_KEY, API_URL } from '../configMovies';
+
 export default {
   name: 'MoviesPage',
 
   components: {
-    
+    //CardTemplate,
   },
-  
+
   data() {
 
     return {
       querry: '',
+      searching: false,
+      firstMovieName: '',
+      image: '',
+      description: '',
     }
 
   },
 
   methods: {
 
+    //FETCH DE PELICULAS POPULARES
     newSearch() {
-      console.log(this.querry)
+
+      this.searching = true
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer ' + API_KEY
+        }
+      };
+      fetch(API_URL + 'movie/popular?language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(movieResults => {
+          this.searching = false
+          console.log(movieResults)
+          if (!movieResults) {
+            this.image = movieResults.results[0].backdrop_path
+            if (movieResults.results[0].overview) {
+              this.description = movieResults.results[0].overview
+            } else {
+              this.description = "ERROR"
+            }
+            this.$parent.Url = movieResults.results[0].strArtistFanart
+          } else {
+            this.image = 'http://4.bp.blogspot.com/-CuZOfJdrDKY/UYmig8q88yI/AAAAAAAAEZM/bzVtIKPhXVA/s1600/Satoshi-nakamoto.gif'
+            this.description = 'Descripción no disponible'
+            this.$parent.Url = 'https://img.freepik.com/free-vector/white-abstract-background_23-2148810113.jpg?size=626&ext=jpg&ga=GA1.2.1991903213.1617148800'
+          }
+        })
+
+        .catch(err => console.error(err));
     }
   },
 
   filters: {
+    formateo(value) {
+      return value.substring(0, 700) + '...'
+    }
+  },
 
+  mounted() {
+    this.newSearch()
   }
 }
 
