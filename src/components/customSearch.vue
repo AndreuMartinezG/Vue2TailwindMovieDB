06c2afb6ca0ea69876f0f353f52b527ff8aca284
“AndreuMartinez” <andreu.martinez@illusionstudio.es>
2023-11-14 21:42:11 +0100

    feat: Creado componente customSearch para proceso de refactorización de MoviesPage.vue

---
+++ src/components/customSearch.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <div class="flex px-4 pt-8 xs:justify-center sm:justify-center md:justify-end lg:justify-end">
+      <select v-model="querry" @change="sendDataToFather" id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-44 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <option disabled selected>Choose an option</option>
+        <option value="popular">Popular</option>
+        <option value="top_rated">Top Rated</option>
+        <option value="upcoming">Upcoming</option>
+        <option value="now_playing">Now Playing</option>
+      </select>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name: 'customSeach',
+
+    methods: {
+        sendDataToFather(){
+            this.$emit('dataFromChild', querry);
+        }
+    }
+}
+
+</script>
--- src/views/MoviesPage.vue
+++ src/views/MoviesPage.vue
@@ -3,10 +3,10 @@
   <div>
 
     <!--TITULO -->
-    <h1 class="font-bold pt-8 text-3xl text-gray-800">{{searchTitle}} MOVIES</h1>
+    <h1 class="font-bold pt-8 text-5xl text-gray-700" :class="{ 'animate-slide-in': !searching, 'hidden': searching }">{{searchTitle}} MOVIES</h1>
 
     <!-- APARTADO SELECT PARA BUSQUEDA-->
-    <div class="flex px-4 pt-8 xs:justify-center sm:justify-center md:justify-end lg:justify-end">
+    <!--<div class="flex px-4 pt-8 xs:justify-center sm:justify-center md:justify-end lg:justify-end">
       <select v-model="querry" @change="newSearch" id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-44 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <option disabled selected>Choose an option</option>
         <option value="popular">Popular</option>
@@ -15,13 +15,15 @@
         <option value="now_playing">Now Playing</option>
       </select>
     </div>
+    -->
+    
 
     <!-- USO DEL COMPONENTE CardTemplate PARA MOSTRAR PELICULAS-->
     <div v-if="!searching" class="grid md:grid-cols-3 gap-4 sm:gird sm:grid-cols-2 sm:gap-2 pt-10 pb-10">
-      <CardTemplate v-for="(movieData, id) in movieResultsData" :key="id" :movie="movieData" />
+      <CardTemplate v-for="(movieData, id) in movieResultsData" :key="id" :movie="movieData" :class="{ 'animate-slide-in': !searching}"/>
     </div>
 
-    <!--PEQUEÑO COMPONENTE DE LOADING MIENTRAS TERMINA EL LLAMADO A LA API-->
+    <!--PEQUEÑO COMPONENTE DE LOADING MIENTRAS TERMINA LA FUNCION DE LLAMADO A LA API-->
     <div v-if="searching" class="w-screen h-auto flex justify-center items-center">
       <h1 class="text-7xl font-bold pt-10">LOADING....</h1>
     </div>
@@ -44,6 +46,7 @@ export default {
 
     return {
       querry: 'popular',
+      pageCounter: 1,
       searchTitle: '',
       searching: false,
       movieData: {},
@@ -61,7 +64,7 @@ export default {
     //FETCH DE PELICULAS DINAMICO 
     newSearch() {
       this.searching = true
-      this.searchTitle = this.convertToUpperCase(this.querry)
+      
       const options = {
         method: 'GET',
         headers: {
@@ -70,10 +73,11 @@ export default {
         }
       };
       console.log(this.querry)
-      fetch(API_URL + 'movie/' + this.querry + '?language=en-US&page=1', options)
+      fetch(API_URL + 'movie/' + this.querry + '?language=en-US&page=' + this.pageCounter, options)
         .then(response => response.json())
         .then(movieResults => {
           this.searching = false
+          this.searchTitle = this.convertToUpperCase(this.querry)
           if (movieResults) {
             this.movieResultsData = movieResults.results
           } else {
@@ -94,3 +98,20 @@ export default {
   }
 }
 </script>
+
+<style>
+
+@keyframes slideIn {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+
+.animate-slide-in {
+  animation: slideIn 1.5s ease-out;
+}
+
+</style>
