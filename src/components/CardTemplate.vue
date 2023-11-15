404448e6d3bf22daf03ec82fd02bad5f61750951
“AndreuMartinez” <andreu.martinez@illusionstudio.es>
2023-11-15 17:08:42 +0100

    fix:Corregidos algunos comentarios en el componente CardTemplate.vue

--- src/components/CardTemplate.vue
+++ src/components/CardTemplate.vue
@@ -4,7 +4,7 @@
     <img class="rounded-t-lg shadow" :src="imgPath" alt="Movie Poster" />
     
     <div class="p-5 flex flex-col flex-grow">
-      
+
       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ movie.title }}</h5>
       <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ movie.overview | formateo }}</p>
 
@@ -42,7 +42,7 @@
           / 10</span>
       </div>
 
-      <!--BOTON PARA ENVIAR LA ID DE LA PELICULA/SERIE AL COMPONENTE PADRE NAVEGAR A "DetailPage.vue" Y REALIZAR EL FETCH AL NUEVO ENDPOINT-->
+      <!--BOTON PARA ENVIAR LA ID DE LA PELICULA/SERIE AL COMPONENTE PADRE-->
       <div>
         <a href="#" @click="sendDataToFather"
           class="w-32 inline-flex justify-center items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
