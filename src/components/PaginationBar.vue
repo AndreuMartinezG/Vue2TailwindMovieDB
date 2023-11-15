820ae6b40650579952a2d2ce5d1958b599004809
“AndreuMartinez” <andreu.martinez@illusionstudio.es>
2023-11-15 21:24:00 +0100

    feat: Exportado codigo a nuevo componente PaginationBar.vue desde vista MoviesPage.vue

--- src/components/PaginationBar.vue
+++ src/components/PaginationBar.vue
@@ -1,6 +1,80 @@
 <template>
-    <div>
+    <div class="flex justify-center py-8">
+        <nav aria-label="Page navigation example">
+            <ul class="flex items-center -space-x-px h-10 text-base">
 
+                <!--BOTÓN PREVIOUS PAGE-->
+                <li>
+                    <a href="#" @click="sendDataToFather(0)" :class="{ 'hidden': minPage }"
+                        class="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white">
+                        <span class="sr-only">Previous</span>
+                        <svg class="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M5 1 1 5l4 4" />
+                        </svg>
+                    </a>
+                </li>
+
+                <!--BOTÓN CON NUMBER PAGE-->
+                <li>
+                    <a href="#" @click="sendDataToFather(1)"
+                        class="flex items-center border justify-center px-4 h-10 leading-tight text-gray-500 bg-white  border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                        :class="{
+                            'dark:bg-gray-700 ms-0 border border-e-0 rounded-s-lg': activePage.page1,
+                            'dark:bg-gray-800': !activePage.page1
+                        }">1
+                    </a>
+                </li>
+                <li>
+                    <a href="#" @click="sendDataToFather(2)"
+                        class="flex items-center border justify-center px-4 h-10 leading-tight text-gray-500 bg-white  border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                        :class="{
+                            'dark:bg-gray-700': activePage.page2,
+                            'dark:bg-gray-800': !activePage.page2
+                        }">2
+                    </a>
+                </li>
+                <li>
+                    <a href="#" @click="sendDataToFather(3)"
+                        class="flex items-center border justify-center px-4 h-10 leading-tight text-gray-500 bg-white  border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                        :class="{
+                            'dark:bg-gray-700': activePage.page3,
+                            'dark:bg-gray-800': !activePage.page3
+                        }">3
+                    </a>
+                </li>
+                <li>
+                    <a href="#" @click="sendDataToFather(4)"
+                        class="flex items-center border justify-center px-4 h-10 leading-tight text-gray-500 bg-white  border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                        :class="{
+                            'dark:bg-gray-700': activePage.page4,
+                            'dark:bg-gray-800': !activePage.page4
+                        }">4</a>
+                </li>
+                <li>
+                    <a href="#" @click="sendDataToFather(5)"
+                        class="flex items-center border justify-center px-4 h-10 leading-tight text-gray-500 bg-white  border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                        :class="{
+                            'dark:bg-gray-700 ms-0 border border-s-0 rounded-e-lg': activePage.page5,
+                            'dark:bg-gray-800': !activePage.page5
+                        }">5</a>
+                </li>
+
+                <!--BOTÓN NEXT PAGE-->
+                <li>
+                    <a href="#" @click="sendDataToFather(6)" :class="{ 'hidden': maxPage }"
+                        class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100  dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white">
+                        <span class="sr-only">Next</span>
+                        <svg class="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 9 4-4-4-4" />
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
@@ -8,6 +82,121 @@
 <script>
 
 export default {
-    name: 'PaginationBar'
+    name: 'PaginationBar',
+
+    props: {
+        pageCounterFather: {
+            type: Number, 
+            require: true
+        }
+    },
+
+    data() {
+        return {
+            pageCounter: 1,
+            maxPage: false,
+            minPage: false,
+            activePage: [
+                this.page1 = false,
+                this.page2 = false,
+                this.page3 = false,
+                this.page4 = false,
+                this.page5 = false
+            ],
+        }
+    },
+
+    methods: {
+
+        //METODO PARA DEVOLVER INFORMACIÓN DE LA PELICULA AL COMPONENTE PADRE
+
+        sendDataToFather(pageNumber) {
+
+            this.paginationProcess(pageNumber)
+
+        },
+
+        //METODO PARA FILTRAR Y SELECCIONAR LA PAGINACIÓN SELECCIONADA
+        paginationProcess(pageNumberSelected) {
+            switch (pageNumberSelected) {
+                case 0:
+                    if (this.pageCounter <= 1) {
+                        this.pageCounter = 1
+                        this.minPage = true
+                    } else {
+                        this.pageCounter = this.pageCounter - 1
+                        this.minPage = false
+                    }
+                    break;
+
+                case 6:
+                    if (this.pageCounter >= 5) {
+                        this.pageCounter = 5
+                        this.maxPage = true
+                    } else {
+                        this.pageCounter = this.pageCounter + 1
+                        this.maxPage = false
+                    }
+                    break;
+
+                default:
+                    this.pageCounter = pageNumberSelected
+
+            }
+            this.pageCounterUpdate(this.pageCounter)
+        },
+        
+        pageCounterUpdate(pageCounter) {
+
+            //ESTAMOS EN LA PAGINA 1 ?
+            if (pageCounter <= 1) {
+                this.minPage = true
+                this.activePage.page1 = true
+            } else {
+                this.minPage = false
+                this.activePage.page1 = false
+            }
+
+            //ESTAMOS EN LA PAGINA 2 ?
+            if (pageCounter == 2) {
+                this.activePage.page2 = true
+            } else {
+                this.activePage.page2 = false
+            }
+
+            //ESTAMOS EN LA PAGINA 3 ?
+            if (pageCounter == 3) {
+                this.activePage.page3 = true
+            } else {
+                this.activePage.page3 = false
+            }
+
+            //ESTAMOS EN LA PAGINA 4 ?
+            if (pageCounter == 4) {
+                this.activePage.page4 = true
+            } else {
+                this.activePage.page4 = false
+            }
+
+            //ESTAMOS EN LA PAGINA 5 ?
+            if (pageCounter >= 5) {
+                this.maxPage = true
+                this.activePage.page5 = true
+            } else {
+                this.maxPage = false
+                this.activePage.page5 = false
+            }
+
+            this.$emit('pageQuerry', { pageQuerry: this.pageCounter });
+        },
+    },
+
+    filters: {
+
+    },
+
+    mounted() {
+        this.pageCounterUpdate(this.pageCounter)
+    }
 }
 </script>
--- src/views/MoviesPage.vue
+++ src/views/MoviesPage.vue
@@ -16,83 +16,8 @@
     </div>
 
     <!--APARTADO PARA EL SISTEMA DE PAGINACIÓN-->
-    <div class="flex justify-center py-8" :class="{ 'hidden': searching }">
-      <nav aria-label="Page navigation example">
-        <ul class="flex items-center -space-x-px h-10 text-base">
+    <PaginationBar @pageQuerry="savePageQuerry" :class="{ 'hidden': searching }" :pageCounter="pageCounter"/>
 
-          <!--BOTÓN PREVIOUS PAGE-->
-          <li >
-            <a href="#" @click="sendDataToFather(0)" :class="{'hidden' : minPage}"
-              class="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white">
-              <span  class="sr-only">Previous</span>
-              <svg class="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="M5 1 1 5l4 4" />
-              </svg>
-            </a>
-          </li>
-
-          <!--BOTÓN CON NUMBER PAGE-->
-          <li>
-            <a href="#" @click="sendDataToFather(1)"
-              class="flex items-center border justify-center px-4 h-10 leading-tight text-gray-500 bg-white  border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              :class=" {
-                'dark:bg-gray-700 ms-0 border border-e-0 rounded-s-lg' : activePage.page1,
-                'dark:bg-gray-800': !activePage.page1
-              }">1
-            </a>
-          </li>
-          <li>
-            <a href="#" @click="sendDataToFather(2)"
-            class="flex items-center border justify-center px-4 h-10 leading-tight text-gray-500 bg-white  border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              :class=" {
-                'dark:bg-gray-700' : activePage.page2,
-                'dark:bg-gray-800': !activePage.page2
-              }">2
-            </a>
-          </li>
-          <li>
-            <a href="#" @click="sendDataToFather(3)"
-            class="flex items-center border justify-center px-4 h-10 leading-tight text-gray-500 bg-white  border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              :class=" {
-                'dark:bg-gray-700' : activePage.page3,
-                'dark:bg-gray-800': !activePage.page3
-              }">3
-            </a>
-          </li>
-          <li>
-            <a href="#" @click="sendDataToFather(4)"
-            class="flex items-center border justify-center px-4 h-10 leading-tight text-gray-500 bg-white  border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              :class=" {
-                'dark:bg-gray-700' : activePage.page4,
-                'dark:bg-gray-800': !activePage.page4
-              }">4</a>
-          </li>
-          <li>
-            <a href="#" @click="sendDataToFather(5)"
-            class="flex items-center border justify-center px-4 h-10 leading-tight text-gray-500 bg-white  border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              :class=" {
-                'dark:bg-gray-700 ms-0 border border-s-0 rounded-e-lg' : activePage.page5,
-                'dark:bg-gray-800': !activePage.page5
-              }">5</a>
-          </li>
-
-          <!--BOTÓN NEXT PAGE-->
-          <li>
-            <a href="#" @click="sendDataToFather(6)" :class="{'hidden' : maxPage}"
-              class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100  dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white">
-              <span class="sr-only">Next</span>
-              <svg class="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="m1 9 4-4-4-4" />
-              </svg>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
   </div>
 </template>
 
@@ -101,14 +26,16 @@
 import CardTemplate from '@/components/CardTemplate.vue';
 import { API_KEY, API_URL } from '../configMovies';
 import CustomSearch from '@/components/CustomSearch.vue';
+import PaginationBar from '@/components/PaginationBar.vue';
 
 export default {
   name: 'MoviesPage',
 
   components: {
     CardTemplate,
-    CustomSearch
-  },
+    CustomSearch,
+    PaginationBar
+},
 
   data() {
 
@@ -117,16 +44,7 @@ export default {
       cardMovieId: '',
 
       pageCounter: 1,
-      maxPage: false,
-      minPage: false,
-      activePage: [
-        this.page1 = false,
-        this.page2 = false,
-        this.page3 = false,
-        this.page4 = false,
-        this.page5 = false
-      ],
-
+      
       searchTitle: '',
       searching: false,
       movieData: {},
@@ -136,85 +54,10 @@ export default {
 
   methods: {
 
-    sendDataToFather(pageNumber){
-
-      this.paginationProcess(pageNumber)
-
-    },
-
-    //METODO PARA FILTRAR Y SELECCIONAR LA PAGINACIÓN SELECCIONADA
-    paginationProcess(pageNumberSelected){
-      switch (pageNumberSelected) {
-        case 0:
-          console.log(this.pageCounter + " PAGE COUNTER")
-          if (this.pageCounter <= 1) {
-            this.pageCounter = 1
-            this.minPage = true
-          }else{
-            this.pageCounter = this.pageCounter - 1
-            this.minPage = false
-          }
-          break;
-
-        case 6:
-          if (this.pageCounter >= 5) {
-            this.pageCounter = 5
-            this.maxPage = true
-          }else{
-            this.pageCounter = this.pageCounter + 1
-            this.maxPage = false
-          }
-          break;
-
-        default:
-          this.pageCounter = pageNumberSelected
-
-      }
-      this.newSearch()
-    },
-
-    //PEQUEÑA FUNCION PARA ACTUALIZAR LA VARIABLES DE PAGINATION BAR
-    pageCounterUpdate(pageCounter){
-      
-      //ESTAMOS EN LA PAGINA 1 ?
-      if (pageCounter <= 1){
-        this.minPage = true
-        this.activePage.page1 = true
-      }else {
-        this.minPage = false
-        this.activePage.page1 = false
-      }
-
-      //ESTAMOS EN LA PAGINA 2 ?
-      if (pageCounter == 2){
-        this.activePage.page2 = true
-      }else {
-        this.activePage.page2 = false
-      }
-
-      //ESTAMOS EN LA PAGINA 3 ?
-      if (pageCounter == 3){
-        this.activePage.page3 = true
-      }else {
-        this.activePage.page3 = false
-      }
-
-      //ESTAMOS EN LA PAGINA 4 ?
-      if (pageCounter == 4){
-        this.activePage.page4 = true
-      }else {
-        this.activePage.page4 = false
-      }
-
-
-      //ESTAMOS EN LA PAGINA 5 ?
-      if (pageCounter >= 5){
-        this.maxPage = true
-        this.activePage.page5 = true
-      }else {
-        this.maxPage = false
-        this.activePage.page5 = false
-      }
+    savePageQuerry(data) {
+      this.pageCounter = data.pageQuerry
+      console.log(this.pageCounter + " PAGE")
+      this.newSearch();
     },
 
     //GUARDAR DATOS RECIBIDOS DEL COMPONENTE HIJO "CustomSearch.vue" Y LLAMADO A LA FUNCION "newSearch()"
@@ -226,7 +69,6 @@ export default {
     ////GUARDAR DATOS RECIBIDOS DEL COMPONENTE HIJO "CardTemplate.vue" Y LLAMADO A LA FUNCION "customSearch()"
     saveCardSearchQuerry(data) {
       this.cardMovieId = data.cardQuerry
-      console.log(this.cardMovieId)
     },
 
     //FUNCION PARA LIMPIAR UNA STRING PARA EL TITULO
@@ -253,7 +95,6 @@ export default {
         .then(movieResults => {
           this.searching = false
           this.searchTitle = this.convertToUpperCase(this.querry)
-          this.pageCounterUpdate(this.pageCounter)
           if (movieResults) {
             this.movieResultsData = movieResults.results
           } else {
