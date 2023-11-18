517a22b611687e5e4b98de4eeff2ead48ac4c6d5
AndreuMartinezG <andreu.martinez.garcia.1992@gmail.com>
2023-11-18 14:39:50 +0100

    feat: Añadido gestor de LocalStorage vue-ls y configurado para su uso dentro de la app

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueTailwind from 'vue-tailwind'
+import VueLocalStorage from 'vue-ls'
+
+// IMPORT ESTILOS CSS
 import './assets/tailwind.css'
 
 import './index.css'
@@ -25,9 +28,19 @@ onMounted(() => {
     initFlowbite();
 })
 
+// CONFIGURACIÓN PARA EL USO DE "LOCALSTORAGE" DENTRO DE LA APP
 
+Vue.use(VueLocalStorage)
 
-// VUE THINGS
+const options = {
+  namespace: 'vuejs__', // Prefijo para evitar colisiones con otros datos en el almacenamiento local
+  name: 'localStorage', // Nombre del objeto en Vue para acceder a los métodos del paquete
+  storage: 'local' // Puede ser 'local' o 'session'
+}
+
+Vue.use(VueLocalStorage, options)
+
+// CONFIGURACIÓN PARA EL USO DE "TAILWIND" DENTRO DE LA APP
 Vue.use(VueTailwind, components)
 
 
