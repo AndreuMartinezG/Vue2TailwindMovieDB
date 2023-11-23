dd0d4ef90222436fde0f2af4864e77c673dc6f3e
AndreuMartinezG <andreu.martinez.garcia.1992@gmail.com>
2023-11-23 12:33:28 +0100

    feat:Empezando diseño de la vista HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,14 +1,17 @@
 
 <template>
-<div></div>
+<div class="flex justify-center items-center md:mx-10 sm:mx-4 sx:mx-4 mx-4">
+    <HomeTitle />
+</div>
 </template>
 
 <script>
+import HomeTitle from '@/components/Home/HomeTitle.vue'
 
 export default {
     name: 'HomeView',
     components: {
-
+        HomeTitle
     },
 
 
